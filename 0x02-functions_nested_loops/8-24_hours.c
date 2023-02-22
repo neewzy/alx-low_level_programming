122c8e2b6291f690d55d440989fe30891bdf22dd
Newton Torkornoo <tiskco@gmail.com>
2023-02-21 17:46:12 -0800

    Jack has more than 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * from 00:00 - 23:59
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int min = 0;
+	int hr = 0;
+
+	while (hr < 24)
+	{
+		while (min < 60)
+		{
+			_putchar('0' + (hr / 10));
+			_putchar('0' + (hr % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		min = 0;
+		hr++;
+	}
+}
