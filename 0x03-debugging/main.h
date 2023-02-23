aa52f7e52b6480b25208d63b9ede83a53ae1530c
Newton Torkornoo <tiskco@gmail.com>
2023-02-23 07:14:36 -0800

    Trail for output

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,11 @@
 void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
+/* returns largest num out of three nums*/
 
+void print_remaining_days(int month, int day, int year);
+/* print remaining days in a year (leap year included)*/
+
+int convert_day(int month, int day);
+/* convert date to days of year*/
 #endif /* MAIN_H */
