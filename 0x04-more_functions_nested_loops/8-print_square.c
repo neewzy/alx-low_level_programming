02754a30e6338e89572cb15f4cf6cdabb8ee418f
Newton Torkornoo <tiskco@gmail.com>
2023-02-23 17:20:43 -0800

    its a square this time

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square 'n' times
+ * if size = 0, prints new line
+ * @size: int argument (num of squares)
+ */
+
+void print_square(int size)
+{
+	int row;
+	int column;
+
+	if (size > 0)
+	{
+		for (column = 1; column <= size; column++)
+		{
+			for (row = 1; row <= size; row++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			row = 1;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
